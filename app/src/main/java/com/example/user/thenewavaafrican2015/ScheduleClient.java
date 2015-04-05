a62d9ca9d13dadd8f56a9c30f42c998aeae106eb
Aidan <aidan.patrick.14@ucl.ac.uk>
2015-04-05 15:21:21 +0100

    Adding the Scheduling class

---
+++ app/src/main/java/com/example/user/thenewavaafrican2015/ScheduleClient.java
@@ -0,0 +1,61 @@
+package com.example.user.thenewavaafrican2015;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+
+import java.util.Calendar;
+
+/**
+ * Created by Aidan on 05/04/2015.
+ */
+public class ScheduleClient {
+
+    private ScheduleService mBoundService;
+    private Context mContext;
+    private boolean mIsBound;
+
+    public ScheduleClient(Context context)
+    {
+        mContext = context;
+    }
+
+    public void doBindService()
+    {
+        mContext.bindService(new Intent(mContext, ScheduleService.class), mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    //Make the connection
+    private ServiceConnection mConnection = new ServiceConnection()
+    {
+        public void onServiceConnected(ComponentName className, IBinder service)
+        {
+            mBoundService = ((ScheduleService.ServiceBinder) service).getService();
+        }
+
+        public void OnServiceDisconnected (ComponentName className)
+        {
+            mBoundService = null;
+        }
+
+
+        //And now the service sets an alarm for a specific date
+
+        public void setAlarmForNotification(Calendar c)
+        {
+            mBoundService.setAlarm(c);
+        }
+
+        public void doUnbindService()
+        {
+            if (mIsBound)
+            {
+                //Detach the connection
+                mContext.unbindService(mConnection);
+                mIsBound = false;
+            }
+        }
+    }
+}
