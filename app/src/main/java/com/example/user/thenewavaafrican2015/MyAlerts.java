b1f4b8a0d8c065c5125a6db418b7d9e8000159e3
Vika Christy <arvika.christy@gmail.com>
2015-04-22 15:08:04 +0100

    Addition for new classes for the last button, still glitch on Aidan's mydosage but successfully loaded on phone

--- app/src/main/java/com/example/user/thenewavaafrican2015/MyAlerts.java
+++ app/src/main/java/com/example/user/thenewavaafrican2015/MyAlerts.java
@@ -1,19 +1,37 @@
 package com.example.user.thenewavaafrican2015;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 
-public class MyAlerts extends ActionBarActivity {
+public class MyAlerts extends ActionBarActivity implements OnClickListener{
+    NotificationManager notif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_alerts);
+        Button reminder = (Button)findViewById(R.id.remindbutton);
+        reminder.setOnClickListener(this);
+        notif = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
     }
 
+    @Override
+    public boolean onClick(View v) {
+        Intent intent = new intent(this, StatusBar.Class);
+        PendingIntent mypending = PendingIntent.getActivity(this, 0, intent, flags);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
